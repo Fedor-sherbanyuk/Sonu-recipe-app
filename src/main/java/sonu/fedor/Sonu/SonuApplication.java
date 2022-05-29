29041b4583800d485420d7cb46cacbc463661a3a
fedor-sherbanyuk <fedor.sherbanyuk1@gmail.com>
2022-05-29 09:58:32 +0300

    SonuNew

---
+++ src/main/java/sonu/fedor/Sonu/SonuApplication.java
@@ -0,0 +1,13 @@
+package sonu.fedor.Sonu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sonu/fedor/Sonu/SonuApplicationTests.java
@@ -0,0 +1,13 @@
+package sonu.fedor.Sonu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
